599050d72d237e508cfcfee45ad025e1a4702194
KatBelyaeva <kat.belyaeva.it@gmail.com>
2023-05-07 22:59:39 +0300

    02-write-file done

---
+++ 02-write-file/index.js
@@ -0,0 +1,15 @@
+const { stdin, stdout, exit } = process;
+const path = require('path');
+const fs = require('fs');
+
+const output = fs.createWriteStream(path.join(__dirname,'destination.txt'));
+
+stdout.write('Привет! Введите текст:\n');
+stdin.on('data', data => {
+  if (data.toString().trim() === 'exit') {
+    stdout.write('До свидания!');
+    exit();
+  } else {
+    output.write(data.toString());
+  }
+});
