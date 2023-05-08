69707fbd10fd2662317f43fd83bc1a0828e603d2
KatBelyaeva <kat.belyaeva.it@gmail.com>
2023-05-08 23:08:27 +0300

    04-copy-directory done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
+const path = require('path');
+const fs = require('fs');
+const copyFile = fs.promises.copyFile;
 
+function copy() {
+  const sourceFolder = path.join(__dirname,'files');
+
+  fs.mkdir(path.join(__dirname,'files-copy'), {
+    recursive: true
+    },
+    (err => {
+      if (err) {
+        throw new Error('Error');
+      }
+      else {
+        console.log('Ok');
+      }
+    })
+  )
+
+  fs.promises.readdir(sourceFolder).then(files => {
+    files.forEach(file => {
+      copyFile(path.join(sourceFolder, file), path.join(__dirname, 'files-copy', file));
+    })
+  })
+}
+
+copy();
