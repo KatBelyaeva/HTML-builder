e65f5b8fbe899b0e9f5d99b806c7fa7dbe53e688
KatBelyaeva <kat.belyaeva.it@gmail.com>
2023-05-09 23:45:53 +0300

    04-copy-directory fix bug

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,24 +5,25 @@ const copyFile = fs.promises.copyFile;
 function copy() {
   const sourceFolder = path.join(__dirname,'files');
 
-  fs.mkdir(path.join(__dirname,'files-copy'), {
-    recursive: true
-    },
-    (err => {
-      if (err) {
-        throw new Error('Error');
-      }
-      else {
-        console.log('Ok');
-      }
+  fs.mkdir(path.join(__dirname,'files-copy'), {recursive: true}, err => {
+      if (err) throw err;
+      console.log('Ok');
+  });
+
+  fs.readdir(path.join(__dirname,'files-copy'), (err, files) => {
+    if (err) throw err;
+    files.forEach(file => {
+      fs.unlink(path.join(__dirname,'files-copy', file), (err) => {
+        if (err) throw err;
+      });
     })
-  )
+  });
 
   fs.promises.readdir(sourceFolder).then(files => {
     files.forEach(file => {
       copyFile(path.join(sourceFolder, file), path.join(__dirname, 'files-copy', file));
     })
-  })
+  });
 }
 
 copy();
